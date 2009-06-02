5613c2d8c82c5676565b53e2ab5df4bb2dceb11e
Rui Guo <firemeteor.guo@gmail.com>
2009-06-02 15:52:37 +0800

    Fix the cmdcallback demo to create the logfile correctly.

--- src/scripts/cmdcallback.lua
+++ src/scripts/cmdcallback.lua
@@ -1,5 +1,6 @@
 --[[ For now, this sample function will simply record all the commands executed ]]--
 function command_executed(name, args)
+  os.execute('mkdir -p /tmp/debug')
   local f = io.open('/tmp/debug/ll', 'a')
   f:write("Command executed: " .. name)
 
