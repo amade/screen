6e928cecf50632f0b35608ef7519fa96f185fa93
Amadeusz Sławiński <amade@asmblr.net>
2010-11-30 14:23:45 +0100

    unused variable

--- src/resize.c
+++ src/resize.c
@@ -327,7 +327,7 @@ kaablamm()
 /* Kills non-resizable layers. */
 #define RESIZE_OR_KILL_LAYERS(l, wi, he) do \
   {	\
-    struct layer *_last = NULL, *_iter;	\
+    struct layer *_last = NULL;	\
     flayer = (l);	\
     while (flayer->l_next)	\
       {	\
