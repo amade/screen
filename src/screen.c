489b0f8e6045e35a8d81ff47d8be405167cfda9e
Amadeusz Sławiński (amade) <amade@asmblr.net>
2011-01-03 12:16:17 +0100

    define functions directly, not by macro

--- src/screen.c
+++ src/screen.c
@@ -2094,8 +2094,6 @@ MakeNewEnv()
   *np = 0;
 }
 
-#define DEFINE_VARARGS_FN(fnname)	void fnname (int err, const char *fmt, VA_DOTS)
-
 #define	PROCESS_MESSAGE(B) do { \
     char *p = B;	\
     VA_LIST(ap)	\
@@ -2113,7 +2111,8 @@ MakeNewEnv()
       }	\
   } while (0)
 
-DEFINE_VARARGS_FN(Msg)
+void Msg(int err, const char *fmt, VA_DOTS)
+
 {
   char buf[MAXPATHLEN*2];
   PROCESS_MESSAGE(buf);
@@ -2148,7 +2147,8 @@ DEFINE_VARARGS_FN(Msg)
 /*
  * Call FinitTerm for all displays, write a message to each and call eexit();
  */
-DEFINE_VARARGS_FN(Panic)
+void Panic(int err, const char *fmt, VA_DOTS)
+
 {
   char buf[MAXPATHLEN*2];
   PROCESS_MESSAGE(buf);
@@ -2205,7 +2205,7 @@ DEFINE_VARARGS_FN(Panic)
   eexit(1);
 }
 
-DEFINE_VARARGS_FN(QueryMsg)
+void QueryMsg(int err, const char *fmt, VA_DOTS)
 {
   char buf[MAXPATHLEN*2];
 
@@ -2216,11 +2216,10 @@ DEFINE_VARARGS_FN(QueryMsg)
   write(queryflag, buf, strlen(buf));
 }
 
-DEFINE_VARARGS_FN(Dummy)
+void Dummy (int err, const char *fmt, VA_DOTS)
 {}
 
 #undef PROCESS_MESSAGE
-#undef DEFINE_VARARGS_FN
 
 /*
  * '^' is allowed as an escape mechanism for control characters. jw.
