18b86c6b7f984d1c9d9351b4ab05e1d96e05e2f3
Amadeusz Sławiński (amade) <amade@asmblr.net>
2011-06-21 13:28:34 +0200

    Revert "little optimization"
    This reverts commit 1fb039dcef6d5965ab8d6b1d38c769a9475a8373.

--- src/process.c
+++ src/process.c
@@ -7626,12 +7626,22 @@ ParseAttrColor(char *s1, char *s2, int msgok)
     {
       int mode = 0, n = 0;
       if (*s == '+')
-	mode = 1;
+	{
+	  mode = 1;
+	  s++;
+	}
       else if (*s == '-')
-	mode = -1;
+	{
+	  mode = -1;
+	  s++;
+	}
       else if (*s == '!')
-	mode = 2;
-      s++;
+	{
+	  mode = 2;
+	  s++;
+	}
+      else if (*s == '=')
+	s++;
       if (*s >= '0' && *s <= '9')
 	{
 	  n = *s++ - '0';
