3e398fba7bb7e3e78fc4200f639a70a4b138a6f4
Amadeusz Sławiński <amade@asmblr.net>
2012-01-10 20:41:26 +0100

    More removal, mainly SYSV SYSVSIGS
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

--- src/os.h
+++ src/os.h
@@ -132,30 +132,7 @@
 #endif
 
 #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
-# if defined(SVR4) && !defined(DGUX) && !defined(__hpux)
-#  include <utmpx.h>
-#  ifdef UTMPX_FILE /* GNU extension */
-#   define UTMPFILE	UTMPX_FILE
-#  endif
-#  define utmp		utmpx
-#  define getutent	getutxent
-#  define getutid	getutxid
-#  define getutline	getutxline
-#  define pututline	pututxline
-#  define setutent	setutxent
-#  define endutent	endutxent
-# else /* SVR4 */
-#  include <utmp.h>
-# endif /* SVR4 */
-# ifdef apollo
-   /* 
-    * We don't have GETUTENT, so we dig into utmp ourselves.
-    * But we save the permanent filedescriptor and
-    * open utmp just when we need to. 
-    * This code supports an unsorted utmp. jw.
-    */
-#  define UTNOKEEP
-# endif /* apollo */
+# include <utmp.h>
 
 # ifndef UTMPFILE
 #  ifdef UTMP_FILE
