581fbf328872a49b12da3f6be19d72ac0414d6e3
Amadeusz Sławiński <amade@asmblr.net>
2012-11-10 15:50:27 +0100

    Fix warning with clang

--- src/ansi.c
+++ src/ansi.c
@@ -1561,7 +1561,7 @@ static void ReverseLineFeed()
 {
 	if (curr->w_y == curr->w_top) {
 		MScrollV(curr, -1, curr->w_top, curr->w_bot, curr->w_rend.colorbg);
-		LScrollV(&curr->w_layer, -1, curr->w_top, curr->w_bot, &curr->w_rend.colorbg);
+		LScrollV(&curr->w_layer, -1, curr->w_top, curr->w_bot, curr->w_rend.colorbg);
 		LGotoPos(&curr->w_layer, curr->w_x, curr->w_y);
 	} else if (curr->w_y > 0)
 		CursorUp(1);
