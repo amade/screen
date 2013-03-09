52f05d6b0ab4357f398cc3005d57ff1ad0f18817
Amadeusz Sławiński <amade@asmblr.net>
2013-03-09 17:52:23 +0100

    Rewrite strlen_onscreen using wcwidth()

--- src/display.c
+++ src/display.c
@@ -1581,20 +1581,9 @@ static void RemoveStatusMinWait()
 
 static int strlen_onscreen(char *c, char *end)
 {
-	int len = 0;
+	int l, len = 0;
 	while (*c && (!end || c < end)) {
-		int v, dec = 0;
-		do {
-			v = FromUtf8(*c++, &dec);
-			if (v == -2)
-				c--;
-		}
-		while (v < 0 && (!end || c < end));
-		if (!utf8_iscomb(v)) {
-			if (utf8_isdouble(v))
-				len++;
-			len++;
-		}
+		len += wcwidth(*c++);
 	}
 
 	return len;
