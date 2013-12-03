4c5ce4bcfe2ee838949c607bdb1f915500d07494
Mike Gerwitz <mike@mikegerwitz.com>
2013-12-02 23:47:07 -0500

    Refactored %t escape sequence into WinTitle handler

--- src/winmsg.h
+++ src/winmsg.h
@@ -48,6 +48,7 @@ typedef enum {
 	WINESC_PID             = 'p',
 	WINESC_COPY_MODE       = 'P', /* copy/_P_aste mode */
 	WINESC_SESS_NAME       = 'S',
+	WINESC_WIN_TITLE       = 't',
 	WINESC_WIN_NAMES       = 'w',
 	WINESC_WIN_NAMES_NOCUR = 'W',
 	WINESC_CMD_ARGS        = 'x',
