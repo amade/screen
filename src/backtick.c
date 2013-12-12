a6c47ca643409e7e926ad65675487c9d38b6a7a0
Mike Gerwitz <mike@mikegerwitz.com>
2013-12-12 12:29:40 -0500

    Moved backtick functions out of winmsg.c and into backtick.c pending refactoring

--- src/backtick.c
+++ src/backtick.c
@@ -32,10 +32,163 @@
  */
 
 #include "backtick.h"
+#include "fileio.h"
 
 /* TODO: get rid of global var */
 Backtick *backticks;
 
+static void backtick_filter(struct backtick *bt)
+{
+	char *p, *q;
+	int c;
+
+	for (p = q = bt->result; (c = (unsigned char)*p++) != 0;) {
+		if (c == '\t')
+			c = ' ';
+		if (c >= ' ' || c == '\005')
+			*q++ = c;
+	}
+	*q = 0;
+}
+
+static void backtick_fn(Event *ev, char *data)
+{
+	struct backtick *bt;
+	int i, j, k, l;
+
+	bt = (struct backtick *)data;
+	i = bt->bufi;
+	l = read(ev->fd, bt->buf + i, MAXSTR - i);
+	if (l <= 0) {
+		evdeq(ev);
+		close(ev->fd);
+		ev->fd = -1;
+		return;
+	}
+	i += l;
+	for (j = 0; j < l; j++)
+		if (bt->buf[i - j - 1] == '\n')
+			break;
+	if (j < l) {
+		for (k = i - j - 2; k >= 0; k--)
+			if (bt->buf[k] == '\n')
+				break;
+		k++;
+		memmove(bt->result, bt->buf + k, i - j - k);
+		bt->result[i - j - k - 1] = 0;
+		backtick_filter(bt);
+		WindowChanged(0, '`');
+	}
+	if (j == l && i == MAXSTR) {
+		j = MAXSTR / 2;
+		l = j + 1;
+	}
+	if (j < l) {
+		if (j)
+			memmove(bt->buf, bt->buf + i - j, j);
+		i = j;
+	}
+	bt->bufi = i;
+}
+
+void setbacktick(int num, int lifespan, int tick, char **cmdv)
+{
+	struct backtick **btp, *bt;
+	char **v;
+
+	for (btp = &backticks; (bt = *btp) != 0; btp = &bt->next)
+		if (bt->num == num)
+			break;
+	if (!bt && !cmdv)
+		return;
+	if (bt) {
+		for (v = bt->cmdv; *v; v++)
+			free(*v);
+		free(bt->cmdv);
+		if (bt->buf)
+			free(bt->buf);
+		if (bt->ev.fd >= 0)
+			close(bt->ev.fd);
+		evdeq(&bt->ev);
+	}
+	if (bt && !cmdv) {
+		*btp = bt->next;
+		free(bt);
+		return;
+	}
+	if (!bt) {
+		bt = malloc(sizeof *bt);
+		if (!bt) {
+			Msg(0, "%s", strnomem);
+			return;
+		}
+		memset(bt, 0, sizeof(*bt));
+		bt->next = 0;
+		*btp = bt;
+	}
+	bt->num = num;
+	bt->tick = tick;
+	bt->lifespan = lifespan;
+	bt->bestbefore = 0;
+	bt->result[0] = 0;
+	bt->buf = 0;
+	bt->bufi = 0;
+	bt->cmdv = cmdv;
+	bt->ev.fd = -1;
+	if (bt->tick == 0 && bt->lifespan == 0) {
+		bt->buf = malloc(MAXSTR);
+		if (bt->buf == 0) {
+			Msg(0, "%s", strnomem);
+			setbacktick(num, 0, 0, (char **)0);
+			return;
+		}
+		bt->ev.type = EV_READ;
+		bt->ev.fd = readpipe(bt->cmdv);
+		bt->ev.handler = backtick_fn;
+		bt->ev.data = (char *)bt;
+		if (bt->ev.fd >= 0)
+			evenq(&bt->ev);
+	}
+}
+
+char *runbacktick(Backtick *bt, int *tickp, time_t now)
+{
+	int f, i, l, j;
+	time_t now2;
+
+	if (bt->tick && (!*tickp || bt->tick < *tickp))
+		*tickp = bt->tick;
+	if ((bt->lifespan == 0 && bt->tick == 0) || now < bt->bestbefore) {
+		return bt->result;
+	}
+	f = readpipe(bt->cmdv);
+	if (f == -1)
+		return bt->result;
+	i = 0;
+	while ((l = read(f, bt->result + i, sizeof(bt->result) - i)) > 0) {
+		i += l;
+		for (j = 1; j < l; j++)
+			if (bt->result[i - j - 1] == '\n')
+				break;
+		if (j == l && i == sizeof(bt->result)) {
+			j = sizeof(bt->result) / 2;
+			l = j + 1;
+		}
+		if (j < l) {
+			memmove(bt->result, bt->result + i - j, j);
+			i = j;
+		}
+	}
+	close(f);
+	bt->result[sizeof(bt->result) - 1] = '\n';
+	if (i && bt->result[i - 1] == '\n')
+		i--;
+	bt->result[i] = 0;
+	backtick_filter(bt);
+	(void)time(&now2);
+	bt->bestbefore = now2 + bt->lifespan;
+	return bt->result;
+}
 
 /* Locate a backtick by its id (number); returns NULL if no such backtick
  * exists. */
