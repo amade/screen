9ae99813bd0850c8edc8de7289d9aa7f5aa00f70
Amadeusz Sławiński <amade@asmblr.net>
2013-12-26 00:30:42 +0100

    other escaped use \033 instead of \E
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

--- src/display.c
+++ src/display.c
@@ -1343,7 +1343,7 @@ void SetColor(uint32_t foreground, uint32_t background)
 
 		ospeed = D_dospeed;
 		/* TODO - properly get escape code */
-		tputs(tparm("\E[38;2;%d;%d;%dm", _r, _g, _b), 1, DoAddChar);
+		tputs(tparm("\033[38;2;%d;%d;%dm", _r, _g, _b), 1, DoAddChar);
 	}
 
 /* BACKGROUND */
@@ -1377,7 +1377,7 @@ void SetColor(uint32_t foreground, uint32_t background)
 
 		ospeed = D_dospeed;
 		/* TODO - properly get escape code */
-		tputs(tparm("\E[48;2;%d;%d;%dm", _r, _g, _b), 1, DoAddChar);
+		tputs(tparm("\033[48;2;%d;%d;%dm", _r, _g, _b), 1, DoAddChar);
 	}
 }
 
