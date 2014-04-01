fc176b9fc6246cb062763b89709e953d474e996c
Amadeusz Sławiński <amade@asmblr.net>
2014-04-02 00:59:03 +0200

    do proper checks if we are on altscreen or not
    fixes screen segfaults with `altscreen on` when using applications making
    use of altscreen like mc when resizing screen window
    
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

--- src/ansi.c
+++ src/ansi.c
@@ -1182,12 +1182,15 @@ static void DoCSI(int c, int intermediate)
 			case 1049:	/*       xterm-like alternate screen */
 				if (use_altscreen) {
 					if (i) {
-						if (!curr->w_alt.on)
+						if (!curr->w_alt.on) {
 							SaveCursor(&curr->w_alt.cursor);
-						EnterAltScreen(curr);
+							EnterAltScreen(curr);
+						}
 					} else {
-						LeaveAltScreen(curr);
-						RestoreCursor(&curr->w_alt.cursor);
+						if (curr->w_alt.on) {
+							LeaveAltScreen(curr);
+							RestoreCursor(&curr->w_alt.cursor);
+						}
 					}
 					if (a1 == 47 && !i)
 						curr->w_saved.on = 0;
