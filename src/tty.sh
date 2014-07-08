e42f3520a7197b58be5dbd3c7a72c81cbf4d19d4
Amadeusz Sławiński <amade@asmblr.net>
2014-07-08 14:56:31 +0200

    Fix job control on DragonFly BSD
    DragonFly BSD needs the same trick as FreeBSD does in fgtty(),
    or else you lose job control inside screen.
    
    From: YONETANI Tomokazu <y0n3t4n1@gmail.com>
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

--- src/tty.sh
+++ src/tty.sh
@@ -812,7 +812,7 @@ int fd;
   /*
    * Under BSD we have to set the controlling terminal again explicitly.
    */
-# if (defined(__FreeBSD_kernel__) || defined(__GNU__)) && defined(TIOCSCTTY)
+# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__GNU__)) && defined(TIOCSCTTY)
   ioctl(fd, TIOCSCTTY, (char *)0);
 # endif
 
