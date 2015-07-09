0e30f04a383bab3813cd83788a219a46e7331c6e
Amadeusz Sławiński <amade@asmblr.net>
2015-07-09 13:05:21 +0200

    remove BUGGYGETLOGIN

--- src/acconfig.h
+++ src/acconfig.h
@@ -215,13 +215,6 @@
 #undef HAVE_UTEMPTER
 
 /*
- * If ttyslot() breaks getlogin() by returning indexes to utmp entries
- * of type DEAD_PROCESS, then our getlogin() replacement should be
- * selected by defining BUGGYGETLOGIN.
- */
-#undef BUGGYGETLOGIN
-
-/*
  * If your system has the calls setreuid() and setregid(),
  * define HAVE_SETREUID. Otherwise screen will use a forked process to
  * safely create output files without retaining any special privileges.
