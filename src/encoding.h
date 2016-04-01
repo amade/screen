6cc25819a7f103d89beccca4e7ad5a08806b4e96
Amadeusz Sławiński <amade@asmblr.net>
2016-04-01 22:25:53 +0200

    ff

--- src/encoding.h
+++ src/encoding.h
@@ -1,5 +1,13 @@
-
-
 enum Encoding {
+	UTF-8,
+	ISO-8859-1,
+	ISO-8859-2
+};
 
-}
+
+struct EncodingHandler {
+	char *name;
+	size_t (* encoder)(const uint32_t mchar, char *buf, size_t bufsize);
+	uint32_t (* decoder)(const char c);
+	size_t (* charwidth)();
+};
