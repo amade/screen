e406ccfd49ed8c3b0c31b7049f200b6940f236af
Amadeusz Sławiński <amade@asmblr.net>
2016-05-22 14:07:50 +0200

    Revert "remove unused file authentication.c"
    This reverts commit 42745c6ac1d803cfc92291e5b18d982aacf8dc4f.

---
+++ src/authentication.c
@@ -0,0 +1,61 @@
+#include <pwd.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "config.h"
+#include "screen.h"
+
+#include "attacher.h"
+
+#if USE_PAM
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
+#endif
+
+bool CheckPassword() {
+#if USE_PAM
+	pam_handle_t *pamh = 0;
+	struct pam_conv pamc;
+	int pam_error;
+	char *tty_name;
+
+	pamc.conv = &misc_conv; 
+	pamc.appdata_ptr = NULL;
+	pam_error = pam_start("screen", ppp->pw_name, &pamc, &pamh);
+	if (pam_error != PAM_SUCCESS) {
+		AttacherFinit(0);  /* goodbye */
+	}
+
+	if (strncmp(attach_tty, "/dev/", 5) == 0) {
+		tty_name = attach_tty + 5;
+	} else {
+		tty_name = attach_tty;
+	}
+	pam_error = pam_set_item(pamh, PAM_TTY, tty_name);
+	if (pam_error != PAM_SUCCESS) {
+		AttacherFinit(0);  /* goodbye */
+	}
+
+	printf("\aScreen used by %s%s<%s> on %s.\n",
+     		ppp->pw_gecos, ppp->pw_gecos[0] ? " " : "", ppp->pw_name, HostName);
+	pam_error = pam_authenticate(pamh, 0);
+	pam_end(pamh, pam_error);
+	if (pam_error == PAM_SUCCESS) {
+		return true;
+	}
+	return false;
+#else
+	return true;
+#endif
+}
+
+void Authenticate() {
+	while (1) {
+		errno = 0;
+
+		if(CheckPassword()) {
+			break;
+		}
+
+	}
+}
