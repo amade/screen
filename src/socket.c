c62bad265d16e63fbe2bb89a50d08bbc5fcbe6ce
Amadeusz Sławiński <amade@asmblr.net>
2017-06-21 16:10:10 +0200

    Revert "remove unneeded password stuff from socket attach/detach"
    This reverts commit 29501d74268a06a34c414bb8de68495f0410e009.
    
    Actually this is correct place to ask user for password, as it is
    handled server side.
    It's bit modified to build, will need also some integration with
    authentication.c to check if password is correct.

--- src/socket.c
+++ src/socket.c
@@ -76,6 +76,7 @@ static void ExecCreate(Message *);
 static void DoCommandMsg(Message *);
 static void FinishAttach(Message *);
 static void FinishDetach(Message *);
+static void AskPassword(Message *);
 
 #define SOCKMODE (S_IWRITE | S_IREAD | (displays ? S_IEXEC : 0) | (multi ? 1 : 0))
 
@@ -815,7 +816,7 @@ void ReceiveMsg()
 	case MSG_ATTACH:
 		if (CreateTempDisplay(&m, recvfd, win))
 			break;
-		FinishAttach(&m);
+		AskPassword(&m);
 		break;
 	case MSG_ERROR:
 		Msg(0, "%s", m.m.message);
@@ -828,7 +829,7 @@ void ReceiveMsg()
 	case MSG_POW_DETACH:
 		if (CreateTempDisplay(&m, recvfd, 0))
 			break;
-		FinishDetach(&m);
+		AskPassword(&m);
 		break;
 	case MSG_QUERY:
 		{
@@ -1069,6 +1070,90 @@ static void FinishDetach(Message *m)
 			FreeDisplay();
 		Kill(pid, SIGCONT);
 	}
+}
+
+static void PasswordProcessInput(char *, size_t);
+
+struct pwdata {
+	int l;
+	char buf[MAXLOGINLEN + 1];
+	Message m;
+};
+
+static void AskPassword(Message *m)
+{
+	struct pwdata *pwdata;
+	pwdata = malloc(sizeof(struct pwdata));
+	if (!pwdata)
+		Panic(0, "%s", strnomem);
+	pwdata->l = 0;
+	pwdata->m = *m;
+	D_processinputdata = (char *)pwdata;
+	D_processinput = PasswordProcessInput;
+	AddStr("Screen password: ");
+}
+
+static void PasswordProcessInput(char *ibuf, size_t ilen)
+{
+	struct pwdata *pwdata;
+	int c, l;
+	char *up;
+	int pid = D_userpid;
+
+	pwdata = (struct pwdata *)D_processinputdata;
+	l = pwdata->l;
+	while (ilen-- > 0) {
+		c = *(unsigned char *)ibuf++;
+		if (c == '\r' || c == '\n') {
+			char *buf = NULL;
+			up = "XXXXXX";
+			pwdata->buf[l] = 0;
+			buf = crypt(pwdata->buf, up);
+			if (!buf || strncmp(buf, up, strlen(up))) {
+				/* uh oh, user failed */
+				memset(pwdata->buf, 0, sizeof(pwdata->buf));
+				if (!buf)
+					AddStr("\r\ncrypt() failed.\r\n");
+				else
+					AddStr("\r\nPassword incorrect.\r\n");
+				D_processinputdata = 0;	/* otherwise freed by FreeDis */
+				FreeDisplay();
+				Msg(0, "Illegal reattach attempt from terminal %s.", pwdata->m.m_tty);
+				free(pwdata);
+				Kill(pid, SIG_BYE);
+				return;
+			}
+			/* great, pw matched, all is fine */
+			memset(pwdata->buf, 0, sizeof(pwdata->buf));
+			AddStr("\r\n");
+			D_processinputdata = 0;
+			D_processinput = ProcessInput;
+			if (pwdata->m.type == MSG_DETACH || pwdata->m.type == MSG_POW_DETACH)
+				FinishDetach(&pwdata->m);
+			else
+				FinishAttach(&pwdata->m);
+			free(pwdata);
+			return;
+		}
+		if (c == Ctrl('c')) {
+			AddStr("\r\n");
+			FreeDisplay();
+			Kill(pid, SIG_BYE);
+			return;
+		}
+		if (c == '\b' || c == 0177) {
+			if (l > 0)
+				l--;
+			continue;
+		}
+		if (c == Ctrl('u')) {
+			l = 0;
+			continue;
+		}
+		if (l < (int)sizeof(pwdata->buf) - 1)
+			pwdata->buf[l++] = c;
+	}
+	pwdata->l = l;
 }
 
 /* 'end' is exclusive, i.e. you should *not* write in *end */
