15b102c15cd63c6749b042854b73e7d2a40483b6
Amadeusz Sławiński <amade@asmblr.net>
2017-07-25 17:50:46 +0200

    clean copyright stuff from help.[ch]
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

--- src/help.h
+++ src/help.h
@@ -6,8 +6,6 @@
 
 void  exit_with_usage (char *, char *, char *);
 void  display_help (char *, struct action *);
-void  display_copyright (void);
-void  display_displays (void);
 void  display_bindkey (char *, struct action *);
 int   InWList (void);
 void  WListUpdatecv (Canvas *, Window *);
