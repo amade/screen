amade/screen

commit 2fab4d6f7372824feb890d6c74b31ffaacfcad09

use size_t in ProcessInput

src/process.c
@@ -599,9 +599,10 @@ static void ClearAction(struct action *act)
  *  everything else on to ProcessInput2.
  */
 
-void ProcessInput(char *ibuf, int ilen)
+void ProcessInput(char *ibuf, size_t ilen)
 {
-	int ch, slen;
+	int ch;
+	size_t slen;
 	unsigned char *s, *q;
 	int i, l;
 	char *p;
@@ -705,7 +706,7 @@ void ProcessInput(char *ibuf, int ilen)
  *  Here only the screen escape commands are handled.
  */
 
-void ProcessInput2(char *ibuf, int ilen)
+void ProcessInput2(char *ibuf, size_t ilen)
 {
 	char *s;
 	int ch;
@@ -726,12 +727,13 @@ void ProcessInput2(char *ibuf, int ilen)
 			slen -= ilen;
 			if (slen)
 				DoProcess(fore, &ibuf, &slen, 0);
-			if (--ilen == 0) {
+			if (ilen == 1) {
 				D_ESCseen = ktab;
 				WindowChanged(fore, WINESC_ESC_SEEN);
+				ilen--;
 			}
 		}
-		if (ilen <= 0)
+		if (ilen == 0)
 			return;
 		ktabp = D_ESCseen ? D_ESCseen : ktab;
 		if (D_ESCseen) {

src/display.h
@@ -83,7 +83,7 @@ struct Display {
 	Canvas *d_cvlist;		/* the canvases of this display */
 	Canvas *d_forecv;		/* current input focus */
 	Layout *d_layout;	/* layout we're using */
-	void (*d_processinput) (char *, int);
+	void (*d_processinput) (char *, size_t);
 	char *d_processinputdata;	/* data for processinput */
 	int d_vpxmin, d_vpxmax;		/* min/max used position on display */
 	Window *d_fore;		/* pointer to fore window */

src/process.h
@@ -4,8 +4,8 @@
 #include "winmsgbuf.h"
 
 void  InitKeytab (void);
-void  ProcessInput (char *, int);
-void  ProcessInput2 (char *, int);
+void  ProcessInput (char *, size_t);
+void  ProcessInput2 (char *, size_t);
 void  DoProcess (Window *, char **, size_t *, struct paster *);
 void  DoAction  (struct action *, int);
 int   FindCommnr (const char *);

commit 9b0243f7ee10cca239d8e1f65cb887e3750272a7

Fix extra characters when sending a command in one go
This closes bug #52372: Giberish printed to screen window in certain
situations.

When sending "^An", most of the time, screen first receives "^A", then
it receives the "n".  This means that in the ProcessInput2 function,
ilen == 1 after the escape character has been seen, ilen is decremented
and the ProcessInput2 function returns.

When "^An" is sent in one go, for example when pasting or when using a
special shortcut, ilen is not 1 when the escape character is seen.  In
this case, the s variable was incremented, but ilen was not decremented.
This leads the function to read an extra character which does not
exists.

This regression was introduced in 2fab4d6f73, as reported in the bug
report.

This can be tested by having a command in the selection:

echo -ne '\x01n' | xclip -i

And pasting it (in this case, you might see the closing bracketed paste
sequence, but that is another problem).

Bug: #52372
Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>

src/process.c
@@ -746,8 +746,9 @@ void ProcessInput2(char *ibuf, size_t ilen)
 			if (ilen == 1) {
 				D_ESCseen = ktab;
 				WindowChanged(fore, WINESC_ESC_SEEN);
-				ilen--;
 			}
+			if (ilen > 0)
+				ilen--;
 		}
 		if (ilen == 0)
 			return;