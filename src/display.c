amade/screen

commit f18f5d0da1d4535c617d0b8d4c6ea7b7288ac623

get rid of fontx
in UTF8 encodings it was used to store part of character
(c << 16), but now that character is ensured to be 32 bits, it's not needed

src/display.c
@@ -431,7 +431,7 @@ static void RAW_PUTCHAR(int c)
 {
 
 	if (D_encoding == UTF8) {
-		c = (c & 255) | (unsigned char)D_rend.font << 8 | (unsigned char)D_rend.fontx << 16;
+		c = (c & 255) | (unsigned char)D_rend.font << 8;
 		if (D_mbcs) {
 			c = D_mbcs;
 			if (D_x == D_width)
@@ -1407,8 +1407,6 @@ void SetRendition(struct mchar *mc)
 		SetColor(mc->colorfg, mc->colorbg);
 	if (D_rend.font != mc->font)
 		SetFont(mc->font);
-	if (D_encoding == UTF8)
-		D_rend.fontx = mc->fontx;
 }
 
 void SetRenditionMline(struct mline *ml, int x)
@@ -1425,8 +1423,6 @@ void SetRenditionMline(struct mline *ml, int x)
 	}
 	if (D_rend.font != ml->font[x])
 		SetFont(ml->font[x]);
-	if (D_encoding == UTF8)
-		D_rend.fontx = ml->fontx[x];
 }
 
 void MakeStatus(char *msg)

src/ansi.c
@@ -64,8 +64,8 @@ struct mline mline_blank;
 struct mline mline_null;
 
 struct mchar mchar_null;
-struct mchar mchar_blank = { ' ', 0, 0, 0, 0, 0, 0 };
-struct mchar mchar_so = { ' ', A_RV, 0, 0, 0, 0, 0};
+struct mchar mchar_blank = { ' ', 0, 0, 0, 0, 0 };
+struct mchar mchar_so = { ' ', A_RV, 0, 0, 0, 0};
 
 uint64_t renditions[NUM_RENDS] = { 65529 /* =ub */ , 65531 /* =b */ , 65533 /* =u */  };
 
@@ -492,7 +492,6 @@ void WriteString(Window *win, char *buf, size_t len)
 						mc.image = c;
 						mc.mbcs = 0;
 						mc.font = '0';
-						mc.fontx = 0;
 						mcp = recode_mchar(&mc, 0, UTF8);
 						c = mcp->image | mcp->font << 8;
 					}
@@ -513,7 +512,7 @@ void WriteString(Window *win, char *buf, size_t len)
 					if (oy < 0)
 						oy = 0;
 					copy_mline2mchar(&omc, &win->w_mlines[oy], ox);
-					if (omc.image == 0xff && omc.font == 0xff && omc.fontx == 0) {
+					if (omc.image == 0xff && omc.font == 0xff) {
 						ox--;
 						if (ox >= 0) {
 							copy_mline2mchar(&omc, &win->w_mlines[oy], ox);
@@ -607,7 +606,6 @@ void WriteString(Window *win, char *buf, size_t len)
 				win->w_rend.image = c;
 				if (win->w_encoding == UTF8) {
 					win->w_rend.font = c >> 8;
-					win->w_rend.fontx = c >> 16;
 				}
 				win->w_rend.mbcs = win->w_mbcs;
 				if (win->w_x < win->w_width - 1) {
@@ -1919,14 +1917,8 @@ static void MFixLine(Window *win, int y, struct mchar *mc)
 			ml->font = null;
 			win->w_FontL = win->w_charsets[win->w_ss ? win->w_ss : win->w_Charset] = 0;
 			win->w_FontR = win->w_charsets[win->w_ss ? win->w_ss : win->w_CharsetR] = 0;
-			mc->font = mc->fontx = win->w_rend.font = 0;
+			mc->font = win->w_rend.font = 0;
 			WMsg(win, 0, "Warning: no space for font - turned off");
-		}
-	}
-	if (mc->fontx && ml->fontx == null) {
-		if ((ml->fontx = calloc(win->w_width + 1, 4)) == 0) {
-			ml->fontx = null;
-			mc->fontx = 0;
 		}
 	}
 	if (mc->colorbg && ml->colorbg == null) {
@@ -2026,9 +2018,6 @@ static void MScrollV(Window *win, int n, int ys, int ye, int bce)
 			if (ml->font != null)
 				free(ml->font);
 			ml->font = null;
-			if (ml->fontx != null)
-				free(ml->fontx);
-			ml->fontx = null;
 			if (ml->colorbg != null)
 				free(ml->colorbg);
 			ml->colorbg = null;
@@ -2062,9 +2051,6 @@ static void MScrollV(Window *win, int n, int ys, int ye, int bce)
 			if (ml->font != null)
 				free(ml->font);
 			ml->font = null;
-			if (ml->fontx != null)
-				free(ml->fontx);
-			ml->fontx = null;
 			if (ml->colorbg != null)
 				free(ml->colorbg);
 			ml->colorbg = null;
@@ -2153,7 +2139,6 @@ static void MInsChar(Window *win, struct mchar *c, int x, int y)
 			ml->font[x + 1] |= 0x80;
 		else if (win->w_encoding == UTF8 && c->mbcs) {
 			ml->font[x + 1] = c->mbcs;
-			ml->fontx[x + 1] = 0;
 		}
 	}
 }
@@ -2175,7 +2160,6 @@ static void MPutChar(Window *win, struct mchar *c, int x, int y)
 			ml->font[x + 1] |= 0x80;
 		else if (win->w_encoding == UTF8 && c->mbcs) {
 			ml->font[x + 1] = c->mbcs;
-			ml->fontx[x + 1] = 0;
 		}
 	}
 }
@@ -2243,12 +2227,6 @@ static void WAddLineToHist(Window *win, struct mline *ml)
 	ml->font = null;
 	if (o != null)
 		free(o);
-	q = ml->fontx;
-	o = hml->fontx;
-	hml->fontx = q;
-	ml->fontx = null;
-	if (o != null)
-		free(o);
 	q = ml->colorbg;
 	o = hml->colorbg;
 	hml->colorbg = q;
@@ -2284,8 +2262,6 @@ int MFindUsedLine(Window *win, int ye, int ys)
 			break;
 		if (win->w_encoding == UTF8) {
 			if (ml->font != null && memcmp(ml->font, null, win->w_width))
-				break;
-			if (ml->fontx != null && memcmp(ml->fontx, null, win->w_width))
 				break;
 		}
 	}

src/encoding.c
@@ -461,8 +461,6 @@ struct mchar *recode_mchar(struct mchar *mc, int from, int to)
 	if (rmc.font == 0)	/* latin1 is the same in unicode */
 		return mc;
 	c = rmc.image | (rmc.font << 8);
-	if (from == UTF8)
-		c |= rmc.fontx << 16;
 	if (rmc.mbcs) {
 		int c2 = rmc.mbcs;
 		c = recode_char_dw_to_encoding(c, &c2, to);
@@ -471,8 +469,6 @@ struct mchar *recode_mchar(struct mchar *mc, int from, int to)
 		c = recode_char_to_encoding(c, to);
 	rmc.image = c & 255;
 	rmc.font = c >> 8 & 255;
-	if (to == UTF8)
-		rmc.fontx = c >> 16 & 255;
 	return &rmc;
 }
 
@@ -485,7 +481,7 @@ struct mline *recode_mline(struct mline *ml, int w, int from, int to)
 
 	if (from == to || (from != UTF8 && to != UTF8) || w == 0)
 		return ml;
-	if (ml->font == null && ml->fontx == null && encodings[from].deffont == 0)
+	if (ml->font == null && encodings[from].deffont == 0)
 		return ml;
 	if (w > maxlen) {
 		for (i = 0; i < 2; i++) {
@@ -497,11 +493,7 @@ struct mline *recode_mline(struct mline *ml, int w, int from, int to)
 				rml[i].font = malloc(w * 4);
 			else
 				rml[i].font = realloc(rml[i].font, w * 4);
-			if (rml[i].fontx == 0)
-				rml[i].fontx = malloc(w * 4);
-			else
-				rml[i].fontx = realloc(rml[i].fontx, w * 4);
-			if (rml[i].image == 0 || rml[i].font == 0 || rml[i].fontx == 0) {
+			if (rml[i].image == 0 || rml[i].font == 0) {
 				maxlen = 0;
 				return ml;	/* sorry */
 			}
@@ -515,8 +507,6 @@ struct mline *recode_mline(struct mline *ml, int w, int from, int to)
 	rl->colorfg = ml->colorfg;
 	for (i = 0; i < w; i++) {
 		c = ml->image[i] | (ml->font[i] << 8);
-		if (from == UTF8)
-			c |= ml->fontx[i] << 16;
 		if (from != UTF8 && c < 256)
 			c |= encodings[from].deffont << 8;
 		if ((from != UTF8 && (c & 0x1f00) != 0 && (c & 0xe000) == 0) || (from == UTF8 && utf8_isdouble(c))) {
@@ -527,8 +517,6 @@ struct mline *recode_mline(struct mline *ml, int w, int from, int to)
 				i++;
 				c2 = ml->image[i] | (ml->font[i] << 8);
 				c = recode_char_dw_to_encoding(c, &c2, to);
-				if (to == UTF8)
-					rl->fontx[i - 1] = c >> 16 & 255;
 				rl->font[i - 1] = c >> 8 & 255;
 				rl->image[i - 1] = c & 255;
 				c = c2;
@@ -537,8 +525,6 @@ struct mline *recode_mline(struct mline *ml, int w, int from, int to)
 			c = recode_char_to_encoding(c, to);
 		rl->image[i] = c & 255;
 		rl->font[i] = c >> 8 & 255;
-		if (to == UTF8)
-			rl->fontx[i] = c >> 16 & 255;
 	}
 	last ^= 1;
 	return rl;
@@ -701,12 +687,10 @@ void WinSwitchEncoding(Window *p, int encoding)
 	flayer = oldflayer;
 	for (j = 0; j < p->w_height + p->w_histheight; j++) {
 		ml = j < p->w_height ? &p->w_mlines[j] : &p->w_hlines[j - p->w_height];
-		if (ml->font == null && ml->fontx == 0 && encodings[p->w_encoding].deffont == 0)
+		if (ml->font == null && encodings[p->w_encoding].deffont == 0)
 			continue;
 		for (i = 0; i < p->w_width; i++) {
 			c = ml->image[i] | (ml->font[i] << 8);
-			if (p->w_encoding == UTF8)
-				c |= ml->fontx[i] << 16;
 			if (p->w_encoding != UTF8 && c < 256)
 				c |= encodings[p->w_encoding].deffont << 8;
 			if (c < 256)
@@ -724,19 +708,8 @@ void WinSwitchEncoding(Window *p, int encoding)
 				else {
 					int c2;
 					i++;
-					c2 = ml->image[i] | (ml->font[i] << 8) | (ml->fontx[i] << 16);
+					c2 = ml->image[i] | (ml->font[i] << 8);
 					c = recode_char_dw_to_encoding(c, &c2, encoding);
-					if (encoding == UTF8) {
-						if (c > 0x10000 && ml->fontx == null) {
-							if ((ml->fontx =
-							     calloc(p->w_width + 1, 4)) == 0) {
-								ml->fontx = null;
-								break;
-							}
-						}
-						ml->fontx[i - 1] = c >> 16 & 255;
-					} else
-						ml->fontx = null;
 					ml->font[i - 1] = c >> 8 & 255;
 					ml->image[i - 1] = c & 255;
 					c = c2;
@@ -745,16 +718,6 @@ void WinSwitchEncoding(Window *p, int encoding)
 				c = recode_char_to_encoding(c, encoding);
 			ml->image[i] = c & 255;
 			ml->font[i] = c >> 8 & 255;
-			if (encoding == UTF8) {
-				if (c > 0x10000 && ml->fontx == null) {
-					if ((ml->fontx = calloc(p->w_width + 1, 4)) == 0) {
-						ml->fontx = null;
-						break;
-					}
-				}
-				ml->fontx[i] = c >> 16 & 255;
-			} else
-				ml->fontx = null;
 		}
 	}
 	p->w_encoding = encoding;
@@ -1167,7 +1130,7 @@ void utf8_handle_comb(unsigned int c, struct mchar *mc)
 	unsigned int i, c1;
 	int isdouble;
 
-	c1 = mc->image | (mc->font << 8) | mc->fontx << 16;
+	c1 = mc->image | (mc->font << 8);
 	isdouble = c1 >= 0x1100 && utf8_isdouble(c1);
 	if (!combchars) {
 		combchars = calloc(0x802, sizeof(struct combchar *));
@@ -1222,7 +1185,6 @@ void utf8_handle_comb(unsigned int c, struct mchar *mc)
 	combchars[i]->c2 = c;
 	mc->image = i & 0xff;
 	mc->font = (i >> 8) + 0xd8;
-	mc->fontx = 0;
 	comb_tofront(root, i);
 }
 

src/image.h
@@ -37,7 +37,6 @@ struct mchar {
 	uint32_t image;		/* actual letter like a, b, c ... as Unicode code point */
 	uint32_t attr;		/* attributes - bold, standout etc. */
 	uint32_t font;		/* font :) */
-	uint32_t fontx; 	/* extended font; TODO: remove - merge with font */
 	uint32_t colorbg;	/* background color */
 	uint32_t colorfg;	/* foreground color */
 	uint32_t mbcs;		/* used for multi byte character sets; TODO: possible to remove? use image now that it has 32 bits*/
@@ -47,7 +46,6 @@ struct mline {
 	uint32_t *image;
 	uint32_t *attr;
 	uint32_t *font;
-	uint32_t *fontx;
 	uint32_t *colorbg;
 	uint32_t *colorfg;
 };
@@ -58,7 +56,6 @@ struct mline {
 	memmove(mline_old.image,   (ml)->image,   (n) * 4);	\
 	memmove(mline_old.attr,    (ml)->attr,    (n) * 4);	\
 	memmove(mline_old.font,    (ml)->font,    (n) * 4);	\
-	memmove(mline_old.fontx,   (ml)->fontx,   (n) * 4);	\
 	memmove(mline_old.colorbg, (ml)->colorbg, (n) * 4);	\
 	memmove(mline_old.colorfg, (ml)->colorfg, (n) * 4);	\
 }
@@ -67,7 +64,6 @@ struct mline {
 	memmove((ml)->image   + (xt), (ml)->image   + (xf), (n) * 4);	\
 	memmove((ml)->attr    + (xt), (ml)->attr    + (xf), (n) * 4);	\
 	memmove((ml)->font    + (xt), (ml)->font    + (xf), (n) * 4);	\
-	memmove((ml)->fontx   + (xt), (ml)->fontx   + (xf), (n) * 4);	\
 	memmove((ml)->colorbg + (xt), (ml)->colorbg + (xf), (n) * 4);	\
 	memmove((ml)->colorfg + (xt), (ml)->colorfg + (xf), (n) * 4);	\
 }
@@ -76,7 +72,6 @@ struct mline {
 	memmove((ml)->image + (x), blank, (n) * 4);				\
 	if ((ml)->attr    != null) memset((ml)->attr    + (x), 0, (n) * 4);	\
 	if ((ml)->font    != null) memset((ml)->font    + (x), 0, (n) * 4);	\
-	if ((ml)->fontx   != null) memset((ml)->fontx   + (x), 0, (n) * 4);	\
 	if ((ml)->colorbg != null) memset((ml)->colorbg + (x), 0, (n) * 4);	\
 	if ((ml)->colorfg != null) memset((ml)->colorfg + (x), 0, (n) * 4);	\
 }
@@ -85,7 +80,6 @@ struct mline {
 	   (ml1)->image[x]   == (ml2)->image[x]		\
 	&& (ml1)->attr[x]    == (ml2)->attr[x]		\
 	&& (ml1)->font[x]    == (ml2)->font[x]		\
-	&& (ml1)->fontx[x]   == (ml2)->fontx[x]		\
 	&& (ml1)->colorbg[x] == (ml2)->colorbg[x]	\
 	&& (ml1)->colorfg[x] == (ml2)->colorfg[x]	\
 )
@@ -94,7 +88,6 @@ struct mline {
 	    (mc1)->image  == (mc2)->image		\
 	&& (mc1)->attr    == (mc2)->attr		\
 	&& (mc1)->font    == (mc2)->font		\
-	&& (mc1)->fontx   == (mc2)->fontx		\
 	&& (mc1)->colorbg == (mc2)->colorbg		\
 	&& (mc1)->colorfg == (mc2)->colorfg		\
 )
@@ -103,7 +96,6 @@ struct mline {
 	   (mc)->image   == (ml)->image[x]		\
 	&& (mc)->attr    == (ml)->attr[x]		\
 	&& (mc)->font    == (ml)->font[x]		\
-	&& (mc)->fontx   == (ml)->fontx[x]		\
 	&& (mc)->colorbg == (ml)->colorbg[x]		\
 	&& (mc)->colorfg == (ml)->colorfg[x]		\
 )
@@ -112,7 +104,6 @@ struct mline {
 	(ml)->image[x]   = (mc)->image;			\
 	(ml)->attr[x]    = (mc)->attr;			\
 	(ml)->font[x]    = (mc)->font;			\
-	(ml)->fontx[x]   = (mc)->fontx;			\
 	(ml)->colorbg[x] = (mc)->colorbg;		\
 	(ml)->colorfg[x] = (mc)->colorfg;		\
 }
@@ -121,7 +112,6 @@ struct mline {
 	(mc)->image   = (ml)->image[x];			\
 	(mc)->attr    = (ml)->attr[x];			\
 	(mc)->font    = (ml)->font[x];			\
-	(mc)->fontx   = (ml)->fontx[x];			\
 	(mc)->colorbg = (ml)->colorbg[x];		\
 	(mc)->colorfg = (ml)->colorfg[x];		\
 	(mc)->mbcs    = 0;				\

src/layer.c
@@ -54,7 +54,6 @@ static struct mline *mlineoffset(const struct mline *ml, const int offset)
 	mml.image = ml->image + offset;
 	mml.attr = ml->attr + offset;
 	mml.font = ml->font + offset;
-	mml.fontx = ml->fontx + offset;
 	mml.colorbg = ml->colorbg + offset;
 	mml.colorfg = ml->colorfg + offset;
 	return &mml;
@@ -298,7 +297,7 @@ void LPutStr(Layer *l, char *s, int n, struct mchar *r, int x, int y)
 			GotoPos(xs2, y2);
 			SetRendition(r);
 			s2 = s + xs2 - x - vp->v_xoff;
-			if (D_encoding == UTF8 && l->l_encoding != UTF8 && (r->font || r->fontx || l->l_encoding)) {
+			if (D_encoding == UTF8 && l->l_encoding != UTF8 && (r->font || l->l_encoding)) {
 				struct mchar mc;
 				mc = *r;
 				while (xs2 <= xe2) {

src/mark.c
@@ -244,7 +244,7 @@ static int rem(int x1, int y1, int x2, int y2, int redisplay, char *pt, int yend
 	uint32_t *im;
 	struct mline *ml;
 	int font;
-	uint32_t *fo, *fox;
+	uint32_t *fo;
 
 	markdata->second = 0;
 	if (y2 < y1 || ((y2 == y1) && (x2 < x1))) {
@@ -285,14 +285,12 @@ static int rem(int x1, int y1, int x2, int y2, int redisplay, char *pt, int yend
 			j--;
 		im = ml->image + j;
 		fo = ml->font + j;
-		fox = ml->fontx + j;
 		font = ASCII;
 		for (; j <= to; j++) {
 			uint32_t c = *im++;
 			uint32_t cf = *fo++;
-			uint32_t cfx = *fox++;
 			if (fore->w_encoding == UTF8) {
-				c |= cf << 8 | cfx << 16;
+				c |= cf << 8;
 				if (c == UCS_HIDDEN)
 					continue;
 				c = ToUtf8_comb(pt, c);
@@ -1098,7 +1096,6 @@ void revto_line(int tx, int ty, int line)
 				mc = mchar_so;
 				if (pastefont) {
 					mc.font = ml->font[x];
-					mc.fontx = ml->fontx[x];
 				}
 				mc.image = ml->image[x];
 			} else
@@ -1199,7 +1196,6 @@ static void MarkRedisplayLine(int y, int xs, int xe, int isblank)
 			break;
 		if (pastefont) {
 			mchar_marked.font = ml->font[x];
-			mchar_marked.fontx = ml->fontx[x];
 		}
 		mchar_marked.image = ml->image[x];
 		mchar_marked.mbcs = 0;

src/resize.c
@@ -58,7 +58,6 @@ static struct mline mline_zero = {
 	.image   = (uint32_t *)0,
 	.attr    = (uint32_t *)0,
 	.font    = (uint32_t *)0,
-	.fontx   = (uint32_t *)0,
 	.colorbg = (uint32_t *)0,
 	.colorfg = (uint32_t *)0
 };
@@ -315,8 +314,6 @@ static void FreeMline(struct mline *ml)
 		free(ml->attr);
 	if (ml->font && ml->font != null)
 		free(ml->font);
-	if (ml->fontx && ml->fontx != null)
-		free(ml->fontx);
 	if (ml->colorbg && ml->colorbg != null)
 		free(ml->colorbg);
 	if (ml->colorfg && ml->colorfg != null)
@@ -329,7 +326,6 @@ static int AllocMline(struct mline *ml, int w)
 	ml->image = malloc(w * 4);
 	ml->attr = null;
 	ml->font = null;
-	ml->fontx = null;
 	ml->colorbg = null;
 	ml->colorfg = null;
 	if (ml->image == 0)
@@ -354,12 +350,6 @@ static int BcopyMline(struct mline *mlf, int xf, struct mline *mlt, int xt, int 
 	}
 	if (mlt->font != null)
 		memmove(mlt->font + xt, mlf->font + xf, l * 4);
-	if (mlf->fontx != null && mlt->fontx == null) {
-		if ((mlt->fontx = calloc(w, 4)) == 0)
-			mlt->fontx = null, r = -1;
-	}
-	if (mlt->fontx != null)
-		memmove(mlt->fontx + xt, mlf->fontx + xf, l * 4);
 	if (mlf->colorbg != null && mlt->colorbg == null) {
 		if ((mlt->colorbg = calloc(w, 4)) == 0)
 			mlt->colorbg = null, r = -1;
@@ -395,10 +385,9 @@ static void CheckMaxSize(int wi)
 	mline_old.image = xrealloc(mline_old.image, maxwidth * 4);
 	mline_old.attr = xrealloc(mline_old.attr, maxwidth * 4);
 	mline_old.font = xrealloc(mline_old.font, maxwidth * 4);
-	mline_old.fontx = xrealloc(mline_old.fontx, maxwidth * 4);
 	mline_old.colorbg = xrealloc(mline_old.colorbg, maxwidth * 4);
 	mline_old.colorfg = xrealloc(mline_old.colorfg, maxwidth * 4);
-	if (!(blank && null && mline_old.image && mline_old.attr && mline_old.font && mline_old.fontx && mline_old.colorbg && mline_old.colorfg))
+	if (!(blank && null && mline_old.image && mline_old.attr && mline_old.font && mline_old.colorbg && mline_old.colorfg))
 		Panic(0, "%s", strnomem);
 
 	MakeBlankLine(blank, maxwidth);
@@ -410,8 +399,6 @@ static void CheckMaxSize(int wi)
 	mline_null.attr = null;
 	mline_blank.font = null;
 	mline_null.font = null;
-	mline_blank.fontx = null;
-	mline_null.fontx = null;
 	mline_blank.colorbg = null;
 	mline_null.colorbg = null;
 	mline_blank.colorfg = null;
@@ -427,7 +414,6 @@ static void CheckMaxSize(int wi)
 	RESET_AFC(ml->image, blank); \
 	RESET_AFC(ml->attr, null); \
 	RESET_AFC(ml->font, null); \
-	RESET_AFC(ml->fontx, null); \
 	RESET_AFC(ml->colorbg, null); \
 	RESET_AFC(ml->colorfg, null); \
       } \

commit 7fc842ddc6e5905da2c5961547e3ff7385bbcc44

fix UTF-8 characters with more than 2 bytes
Characters are stored as uint32 nowadays. Looking at the lower bytes
only seems to be an oversight from the fontx clean up.

Fixes: f18f5d0da1d4 get rid of fontx

src/display.c
@@ -431,7 +431,6 @@ static void RAW_PUTCHAR(uint32_t c)
 {
 
 	if (D_encoding == UTF8) {
-		c = (c & 255) | (unsigned char)D_rend.font << 8;
 		if (D_mbcs) {
 			c = D_mbcs;
 			if (D_x == D_width)